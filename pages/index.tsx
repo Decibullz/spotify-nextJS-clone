fa0a8a64406d42c5e20024fc17992c5b5758e188
Cody Snell <codyjsnell@yahoo.com>
2022-02-04 23:49:25 -0600

    adds center component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
-import Head from 'next/head'
 import Sidebar from '../components/Sidebar'
+import Center from '../components/Center'
 
 export default function Home() {
   return (
-    <div className="">
-      <main className='bg-black h-screen overflow-hidden'>
+    <div className="bg-black h-screen overflow-hidden'">
+      <main className='flex'>
          <Sidebar/>
-        {/* center */}
+        <Center/>
       </main>
       <div>
         {/* player */}

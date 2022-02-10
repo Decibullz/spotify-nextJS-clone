40fd52fb9452fe41f26966314a09e06a041c8edf
Cody Snell <codyjsnell@yahoo.com>
2022-02-10 13:25:03 -0600

    file type change tsx >js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,16 @@
 import '../styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
-import type { AppProps } from 'next/app'
-import {RecoilRoot} from 'recoil'
+// import type { AppProps } from 'next/app'
+import { RecoilRoot } from 'recoil'
 
-function MyApp({ Component, pageProps:{session, ...pageProps} }: AppProps) {
-  return(
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
     <SessionProvider session={session}>
       <RecoilRoot>
-      <Component {...pageProps} />
+        <Component {...pageProps} />
       </RecoilRoot>
     </SessionProvider>
-  ) 
+  )
 }
 
 export default MyApp

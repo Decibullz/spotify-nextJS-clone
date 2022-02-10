f62df836b3ecad9d9aeaf830c0c74412aecde8de
Cody Snell <codyjsnell@yahoo.com>
2022-02-10 16:03:48 -0600

    removes unused import

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import '../styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
-// import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {

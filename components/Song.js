a1237b53dfc0b291fd9158cebe955e463837dd8b
Cody Snell <codyjsnell@yahoo.com>
2022-02-10 14:47:38 -0600

    plays selected song on click, sets song to state

--- components/Song.js
+++ components/Song.js
@@ -1,11 +1,26 @@
 import useSpotify from '../hooks/useSpotify'
 import { time } from '../lib/time'
+import { currentTrackIdState, isPlayingState } from '../atoms/songAtom'
+import { useRecoilState } from 'recoil'
 
 function Song({ order, track }) {
   const spotifyApi = useSpotify()
+  const [currentTrackId, setCurrentTrackId] =
+    useRecoilState(currentTrackIdState)
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState)
+  const playSong = () => {
+    setCurrentTrackId(track.track.id)
+    setIsPlaying(true)
+    spotifyApi.play({
+      uris: [track.track.uri],
+    })
+  }
   console.log(track)
   return (
-    <div className=" grid cursor-pointer grid-cols-2 rounded-lg py-4 px-5 text-gray-500 hover:bg-gray-900">
+    <div
+      onClick={playSong}
+      className=" grid cursor-pointer grid-cols-2 rounded-lg py-4 px-5 text-gray-500 hover:bg-gray-900"
+    >
       <div className="flex items-center space-x-4">
         <p>{order + 1}</p>
         <img

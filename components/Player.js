d8786caff75965ebd991ec561ff7e3b59ebf82c5
Cody Snell <codyjsnell@yahoo.com>
2022-02-10 16:06:11 -0600

    dynamic rendering, adds icons, pause/play functionality,

--- components/Player.js
+++ components/Player.js
@@ -1,7 +1,18 @@
+import {
+  RewindIcon,
+  SwitchHorizontalIcon,
+  FastForwardIcon,
+  PauseIcon,
+  PlayIcon,
+  ReplyIcon,
+  VolumeUpIcon,
+} from '@heroicons/react/solid'
+import { VolumeUpIcon as VolumeDownIcon } from '@heroicons/react/outline'
 import { useSession } from 'next-auth/react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil'
 import { currentTrackIdState, isPlayingState } from '../atoms/songAtom'
+import useSongInfo from '../hooks/useSongInfo'
 import useSpotify from '../hooks/useSpotify'
 
 function Player() {
@@ -11,11 +22,71 @@ function Player() {
     useRecoilState(currentTrackIdState)
   const [isPlaying, setIsplaying] = useRecoilState(isPlayingState)
   const [volume, setVolume] = useState(50)
+  const songInfo = useSongInfo()
+
+  const fetchCurrentSong = () => {
+    if (!songInfo) {
+      spotifyApi.getMyCurrentPlayingTrack().then((data) => {
+        setCurrentTrackId(data.body?.item?.id)
+
+        spotifyApi.getMyCurrentPlaybackState().then((data) => {
+          setIsplaying(data.body?.is_playing)
+        })
+      })
+    }
+  }
+
+  const handlePlayPause = () => {
+    spotifyApi.getMyCurrentPlaybackState().then((data) => {
+      console.log(data)
+      if (data.body.is_playing) {
+        spotifyApi.pause()
+        setIsplaying(false)
+      } else {
+        spotifyApi.play()
+        setIsplaying(true)
+      }
+    })
+  }
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken() && !currentTrackId) {
+      fetchCurrentSong()
+      setVolume(50)
+    }
+  }, [currentTrackIdState, spotifyApi, session])
+
   return (
-    <div>
+    <div className="grid h-24 grid-cols-3 bg-gradient-to-b from-black to-gray-900 px-2 text-xs text-white md:px-8 md:text-base">
       {/* left */}
-      <div>
-        <img src="" alt="" />
+      <div className="flex items-center space-x-4">
+        <img
+          className="hidden h-10 w-10 md:inline"
+          src={songInfo?.album.images?.[0].url}
+          alt=""
+        />
+        <div>
+          <h3>{songInfo?.name}</h3>
+          <p>{songInfo?.artists?.[0]?.name}</p>
+        </div>
+      </div>
+      {/* center */}
+      <div className="flex items-center justify-evenly">
+        <SwitchHorizontalIcon className="button" />
+        <RewindIcon
+          // onClick={() => spotifyApi.skipToPrevious()}
+          className="button"
+        />
+        {isPlaying ? (
+          <PauseIcon onClick={handlePlayPause} className="button" />
+        ) : (
+          <PlayIcon onClick={handlePlayPause} className="button" />
+        )}
+        <FastForwardIcon
+          // onClick={() => spotifyApi.skipToNext()}
+          className="button"
+        />
+        <ReplyIcon className="button" />
       </div>
     </div>
   )
